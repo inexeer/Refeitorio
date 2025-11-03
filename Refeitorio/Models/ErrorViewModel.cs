332623fd5c1572c838b582784139a9bfe994e4f9
inexeer <135962360+inexeer@users.noreply.github.com>
2025-11-03 16:40:22 +0000

    Add project files.

---
+++ Refeitorio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Refeitorio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
